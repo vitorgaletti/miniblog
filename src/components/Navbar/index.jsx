d80cf8f027f655f8acab56c1a2c853971b0b8fc4
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-04-26 15:08:19 -0300

    :lipstick: CSS navbar and footer

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,18 +4,28 @@ import styles from './styles.module.css';
 
 export function Navbar() {
   return (
-    <div>
-      <NavLink to="/">
+    <nav className={styles.navbar}>
+      <NavLink to="/" className={styles.brand}>
         Mini <span>Blog</span>
       </NavLink>
-      <ul>
+      <ul className={styles.links_list}>
         <li>
-          <NavLink to="/">Home</NavLink>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? styles.active : '')}
+          >
+            Home
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/about">Sobre</NavLink>
+          <NavLink
+            to="/about"
+            className={({ isActive }) => (isActive ? styles.active : '')}
+          >
+            Sobre
+          </NavLink>
         </li>
       </ul>
-    </div>
+    </nav>
   );
 }
